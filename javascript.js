f95c98d10aefda741423aefe935fe3310dbc492d
MohamedAbdel-Azeem <mohamedk.abdelazeem@gmail.com>
2023-10-30 15:39:32 +0200

    Pixel Size fixed when changing dimension

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 const gridContainer = document.querySelector('#gridContainer');
 const paintButton = document.querySelector('#brush');
 const eraseButton = document.querySelector('#eraser');
+const clearButton = document.querySelector('#clear');
 let artMode = 'brush';
 let colorCode = '#000000';
 let paintMode = 'click';
+let dimension = 16;
+const gridSize = 600;
 
 Main();
 
 
-function drawGrid(dimension){
+function drawGrid(){
     let counter = 0;
     for (let i = 0; i < dimension ; i++){
         const col = document.createElement('div');
@@ -18,6 +21,7 @@ function drawGrid(dimension){
             let gridPixel = document.createElement('div');
             gridPixel.classList.add('pixel');
             gridPixel.setAttribute('id',counter);
+            gridPixel.style.padding = getPadding();
             gridPixel.addEventListener('click',()=>{
                 if (artMode == 'brush'){
                     gridPixel.style.backgroundColor = colorCode;
@@ -36,6 +40,15 @@ function pickColor(){
 
 }
 
+function getPadding(){
+    switch (dimension){
+        case 32 : return '8px';
+        case 8 : return '32px';
+        case 4 : return '64px';
+        case 64 : return '4px';
+        default : return '16px';
+    }
+}
 
 
 function Main(){
@@ -45,7 +58,8 @@ function Main(){
     eraseButton.addEventListener('click',()=>{
         artMode = 'eraser';
     });
-    drawGrid(16);
+
+    drawGrid();
     pickColor();
 }
 
